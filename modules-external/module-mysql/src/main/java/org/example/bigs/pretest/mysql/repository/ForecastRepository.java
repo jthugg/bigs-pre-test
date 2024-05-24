a3cea7709ed6e74b0f37e5edf9ea388aca9f1893
Hi. I'm Neo <jthug@naver.com>
2024-05-24 17:25:57 +0900

    feat: 데이터베이스 접근 메서드 추가

--- modules-external/module-mysql/src/main/java/org/example/bigs/pretest/mysql/repository/ForecastRepository.java
+++ modules-external/module-mysql/src/main/java/org/example/bigs/pretest/mysql/repository/ForecastRepository.java
@@ -4,6 +4,11 @@ import org.example.bigs.pretest.mysql.entity.ForeCast;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+
 @Repository
 public interface ForecastRepository extends JpaRepository<ForeCast, Long> {
+
+    boolean existsByAnnouncedAt(Instant announcedAt);
+
 }
